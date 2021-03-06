4cc6642d2316b86970bfc2ca44bf289501e0b9bf
Philipp Engler <developer@p-engler.com>
2021-03-06 15:12:53 +0100

    changed ip addresses for p2p external connections

--- src/eufySecurityApi/p2p/cloud-lookup.service.ts
+++ src/eufySecurityApi/p2p/cloud-lookup.service.ts
@@ -7,9 +7,12 @@ import { promiseAny } from '../http/http.utils';
 export class CloudLookupService {
   private readonly addressTimeoutInMs = 3 * 1000;
   private addresses: Array<Address> = [
-    { host: '54.223.148.206', port: 32100 },
-    { host: '18.197.212.165', port: 32100 },
-    { host: '13.251.222.7', port: 32100 },
+    { host: "18.197.212.165", port: 32100 },
+    { host: "34.235.4.153", port: 32100 },
+    { host: "54.153.101.7", port: 32100 },
+    { host: "18.223.127.200", port: 32100 },
+    { host: "54.223.148.206", port: 32100 },
+    { host: "13.251.222.7", port: 32100 },
   ];
 
   private async bind(socket: Socket): Promise<void> {
