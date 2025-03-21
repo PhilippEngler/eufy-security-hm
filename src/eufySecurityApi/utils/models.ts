a8b1dd10fe252736b7ac94be367b294c36531174
Philipp Engler <developer@p-engler.com>
2025-03-21 17:10:07 +0100

    added HM sysvar types

--- src/eufySecurityApi/utils/models.ts
+++ src/eufySecurityApi/utils/models.ts
@@ -41,3 +41,67 @@ export interface CameraEvent {
     path: string;
     start_time: Date;
 }
+
+/**
+ * The types for the value types of system variables in HomeMatic
+ */
+export type HomeMaticSystemvariableValueType = "ivtBinary" | "ivtFloat" | "ivtInteger" | "ivtString";
+
+/**
+ * The types for the value sub types of system variables in HomeMatic
+ */
+export type HomeMaticSystemvariableValueSubType = "istPresent" | "istAlarm" | "istGeneric" | "istBool" | "istEnum" | "istChar8859";
+
+/**
+ * The interface for common system variables in HomeMatic
+ */
+export interface HomeMaticSystemvariable {
+    name: string;
+    info: string;
+    valueType: HomeMaticSystemvariableValueType;
+    valueSubType: HomeMaticSystemvariableValueSubType;
+}
+
+/**
+ * The interface for string system variables in HomeMatic.
+ */
+export interface HomeMaticSystemvariableString extends HomeMaticSystemvariable {
+    valueType: "ivtString";
+    valueSubType: "istChar8859";
+    valueUnit: "";
+    state: "";
+}
+
+/**
+ * The interface for float system variables in HomeMatic.
+ */
+export interface HomeMaticSystemvariableFloat extends HomeMaticSystemvariable {
+    valueType: "ivtFloat";
+    valueSubType: "istGeneric";
+    valueUnit: string;
+    valueMin: number;
+    valueMax: number,
+    state: number;
+}
+
+/**
+ * The interface for binary system variables in HomeMatic.
+ */
+export interface HomeMaticSystemvariableBinary extends HomeMaticSystemvariable {
+    valueType: "ivtBinary";
+    valueSubType: "istBool";
+    valueUnit: "";
+    valueName0: string;
+    valueName1: string;
+    state: boolean;
+}
+
+/**
+ * The interface for integer system variables in HomeMatic.
+ */
+export interface HomeMaticSystemvariableInteger extends HomeMaticSystemvariable {
+    valueType: "ivtInteger";
+    valueSubType: "istEnum";
+    valueList: string;
+    state: number;
+}
