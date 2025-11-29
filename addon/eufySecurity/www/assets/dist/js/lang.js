3b7698673e8011bb3ae7a37cec9fa14011800895
Philipp Engler <developer@p-engler.com>
2025-11-29 10:19:51 +0100

    solved a problem with setting language manually

--- addon/eufySecurity/www/assets/dist/js/lang.js
+++ addon/eufySecurity/www/assets/dist/js/lang.js
@@ -7,7 +7,7 @@ function getLanguage() {
     var lang = "en";
     var urlParams = new URLSearchParams(window.location.search);
     if(getParameterFromURLSearchParams(urlParams, "lang")) {
-		lang = urlParams.get(lang);
+		lang = urlParams.get("lang");
 	} else {
         lang = navigator.language.slice(0,2);
     }
